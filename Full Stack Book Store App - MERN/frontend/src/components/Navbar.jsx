d86f64b72a6f64e555ab97e3a3920a4bdacc4ea8
Isuru <isuruindrajith680@gmail.com>
2024-11-06 16:46:33 +0530

    Navbar implemented

--- Full Stack Book Store App - MERN/frontend/src/components/Navbar.jsx
+++ Full Stack Book Store App - MERN/frontend/src/components/Navbar.jsx
@@ -1,20 +1,40 @@
 import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa6";
 import { IoSearchOutline } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa6";
+import { MdOutlineShoppingCartCheckout } from "react-icons/md";
+
+import avatarImg from "../assets/avatar.png"
+import { useState } from "react";
+
+const navigation = [
+    {name: "Dashboard", href:"/dashboard"},
+    {name: "Orders", href:"/order"},
+    {name: "Cart Page", href:"/cart"},
+    {name: "Check Out", href:"/checkout"},
+]
 
 const Navbar = () => {
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+  
+
+    const currentUser = true;
     return (
         <header className="max-w-screen-2xl mx-auto px-4 py-6">
             <nav className = "flex justify-between items-center">
                 {/* left side */}
-                <div>
+                <div className="flex items-center md:gap-16 gap-4">
                     <Link to="/">
                     <FaBars className="size-6"/>
                     </Link>
 
                     {/* search input */}
-                    <div>
-                    <IoSearchOutline />
+                    <div className="relative sm:w-72 w-40 space-x-2">
+
+                    <IoSearchOutline className="absolute inline-block left-3 inset-y-2" />
+
                     <input type="text" placeholder="Search here" 
                     className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md
                     focus:outline-none"
@@ -23,8 +43,51 @@ const Navbar = () => {
                 </div>
 
                 {/*right side*/}
-                <div>
-                    nav items:
+                <div className="relative flex items-center mn:space-x-3 space-x-2">
+                    <div>
+                         {
+                            currentUser ? (<>
+                            <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                                 <img src={avatarImg} alt="" className={`size-7 rounded-full ${currentUser ? 'ring-2 ring-blue-500' : ''}`} />
+                            </button> 
+                            {/* show dropdown */}
+                            {
+                                isDropdownOpen && (
+                                    <div className="absolute right-0 mt-2 w-48 bg-white
+                                    shadow-lg rounded-md z-40"> 
+                                         <ul className="py-2">
+                                            {
+                                                navigation.map((item) => (
+                                                    <li key={item.name} onClick={() => setIsDropdownOpen(false)}>
+                                                        <Link to={item.href} className="block
+                                                        px-4 py-2 text-sm hover:bg-gray-100">
+                                                            {item.name}
+                                                        </Link>
+                                                    </li>
+                                                ))
+                                            }
+                                        </ul>
+                                    </div>
+
+                                )
+                            }
+            </>
+        ) : (
+            <Link to="/login"><FaRegUser className="size-6" /></Link>
+        )
+    }
+</div>
+
+                    
+                    <button className="hidden sm:block">
+                        <FaRegHeart className="size-6"/>
+                    </button>
+
+                <Link to="/cart" className="bg-primary p1 sm:px-6 px-2 flex items-center
+                rounded-sm">
+                    <MdOutlineShoppingCartCheckout className="size-6"/>
+                    <span className="text-sm font-semibold sm:ml-1">0</span>
+                </Link>
                 </div>
             </nav>
         </header>
