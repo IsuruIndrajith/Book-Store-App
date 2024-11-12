319161bb6e24a110abfa31165ed5ba8e98df64c8
Isuru <isuruindrajith680@gmail.com>
2024-11-13 01:08:51 +0530

    Cart actions

--- Full Stack Book Store App - MERN/frontend/src/routers/router.jsx
+++ Full Stack Book Store App - MERN/frontend/src/routers/router.jsx
@@ -3,6 +3,7 @@ import App from "../App";
 import Home from "../pages/home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import CartPage from "../pages/books/CartPage";
 
 const router = createBrowserRouter([
     {
@@ -28,8 +29,12 @@ const router = createBrowserRouter([
         {
           path: "/register",
           element:<Register/>
+        },
+        {
+          path: "/cart",
+          element: <CartPage/>
         }
-
+          
       ]
     },
   ]);
