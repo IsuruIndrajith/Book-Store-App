45d83b30f46e86dbf6f7ad1d876096ac19c87ff4
Isuru <isuruindrajith680@gmail.com>
2024-11-15 18:04:03 +0530

    Login and Logout

--- Full Stack Book Store App - MERN/frontend/src/components/Navbar.jsx
+++ Full Stack Book Store App - MERN/frontend/src/components/Navbar.jsx
@@ -8,6 +8,7 @@ import { MdOutlineShoppingCartCheckout } from "react-icons/md";
 import avatarImg from "../assets/avatar.png"
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { useAuth } from "../context/AuthContext";
 
 const navigation = [
     {name: "Dashboard", href:"/dashboard"},
@@ -21,8 +22,11 @@ const Navbar = () => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false)
     const cartItems =useSelector(state => state.cart.cartItems)
     
-
-    const currentUser = false;
+    const {currentUser, logout} = useAuth()
+    const handleLogout = () => {
+        logout()
+    }
+    
     return (
         <header className="max-w-screen-2xl mx-auto px-4 py-6">
             <nav className = "flex justify-between items-center">
@@ -68,6 +72,12 @@ const Navbar = () => {
                                                     </li>
                                                 ))
                                             }
+                                            <li>
+                                                <button  
+                                                onClick={handleLogout}
+                                                className="block w-full text-left
+                                                px-4 py-2 text-sm hover:bg-gray-100"> LogOut </button>
+                                            </li>
                                         </ul>
                                     </div>
 
