981f1722c362a12f343c628eabe69f83115e8e9c
Isuru <isuruindrajith680@gmail.com>
2024-11-17 11:41:40 +0530

    Admin Login

--- Full Stack Book Store App - MERN/frontend/src/routers/router.jsx
+++ Full Stack Book Store App - MERN/frontend/src/routers/router.jsx
@@ -8,6 +8,8 @@ import CheckoutPage from "../pages/books/CheckoutPage";
 import SingleBook from "../pages/books/SingleBook";
 import PrivateRoute from "./PrivateRoute";
 import OrderPage from "../pages/books/OrderPage";
+import AdminRoute from "./AdminRoute";
+import AdminLogin from "../components/AdminLogin";
 
 
 const router = createBrowserRouter([
@@ -50,5 +52,27 @@ const router = createBrowserRouter([
           
       ]
     },
+    {
+      path: "/admin",
+      element: <AdminLogin/>
+
+    },
+    {
+      path: "/dashboard",
+      element: <AdminRoute><div>Admin Dashboard</div></AdminRoute>
+    },
+    {
+      path: "add-new-book",
+      element: <AdminRoute><div>Add New Book</div></AdminRoute>
+    },
+    {
+      path: "edit-book/:id",
+      element: <AdminRoute><div>Edit Book</div></AdminRoute>
+    },
+    {
+      path: "manage-book",
+      element: <AdminRoute><div>Manage Books</div></AdminRoute>
+    },
+
   ]);
   export default router;
