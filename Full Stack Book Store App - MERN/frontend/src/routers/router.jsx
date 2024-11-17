786a1f00e711f3f307e9d6325eb9975510214b71
Isuru <isuruindrajith680@gmail.com>
2024-11-17 13:56:50 +0530

    Dashboard and dashboard layout

--- Full Stack Book Store App - MERN/frontend/src/routers/router.jsx
+++ Full Stack Book Store App - MERN/frontend/src/routers/router.jsx
@@ -10,6 +10,8 @@ import PrivateRoute from "./PrivateRoute";
 import OrderPage from "../pages/books/OrderPage";
 import AdminRoute from "./AdminRoute";
 import AdminLogin from "../components/AdminLogin";
+import Dashboard from "../pages/dashboard/Dashboard";
+import DashboardLayout from "../pages/dashboard/DashboardLayout";
 
 
 const router = createBrowserRouter([
@@ -59,20 +61,33 @@ const router = createBrowserRouter([
     },
     {
       path: "/dashboard",
-      element: <AdminRoute><div>Admin Dashboard</div></AdminRoute>
-    },
-    {
-      path: "add-new-book",
-      element: <AdminRoute><div>Add New Book</div></AdminRoute>
-    },
-    {
-      path: "edit-book/:id",
-      element: <AdminRoute><div>Edit Book</div></AdminRoute>
-    },
-    {
-      path: "manage-book",
-      element: <AdminRoute><div>Manage Books</div></AdminRoute>
-    },
-
+      element: <AdminRoute>
+        <DashboardLayout/>
+      </AdminRoute>,
+      children:[
+        {
+          path: "",
+          element: <AdminRoute><Dashboard/></AdminRoute>
+        },
+        {
+          path: "add-new-book",
+          element: <AdminRoute>
+            
+          </AdminRoute>
+        },
+        {
+          path: "edit-book/:id",
+          element: <AdminRoute>
+           
+          </AdminRoute>
+        },
+        {
+          path: "manage-books",
+          element: <AdminRoute>
+            
+          </AdminRoute>
+        }
+      ]
+    }
   ]);
   export default router;
